28ebf38a4738a2f586347f029e3bb28ffebc0ee9
Michael Ludvig <mludvig@logix.net.nz>
2020-05-04 17:09:24 +1200

    Added MIT license from aws-samples/amazon-textract-code-samples
    https://github.com/aws-samples/amazon-textract-code-samples/blob/7bcc382/LICENSE

--- trp/__init__.py
+++ trp/__init__.py
@@ -1,3 +1,18 @@
+# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 __version__ = '0.1.0'
 
 class BoundingBox:
@@ -276,7 +291,7 @@ class FieldValue:
     @property
     def text(self):
         return self._text
-    
+
     @property
     def block(self):
         return self._block
@@ -340,7 +355,7 @@ class Form:
         if(key in self._fieldsMap):
             field = self._fieldsMap[key]
         return field
-    
+
     def searchFieldsByKey(self, key):
         searchKey = key.lower()
         results = []
